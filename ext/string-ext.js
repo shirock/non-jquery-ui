f07243aec343e91069329bc70e256d4ca9085905
rock <shirock@seed.net.tw>
2018-11-23 17:17:02 +0800

    add String.startsWith, String.endsWith.

--- ext/string-ext.js
+++ ext/string-ext.js
@@ -74,3 +74,18 @@ String.prototype.trim = function(){
     return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
 }
 }
+
+if (!String.prototype.startsWith) {
+String.prototype.startsWith = function(search, pos) {
+    return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+};
+}
+
+if (!String.prototype.endsWith) {
+String.prototype.endsWith = function(search, this_len) {
+    if (this_len === undefined || this_len > this.length) {
+        this_len = this.length;
+    }
+    return this.substring(this_len - search.length, this_len) === search;
+};
+}
