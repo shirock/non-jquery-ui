d72c3427664a4a5fc8ac139f42c5609d2e49bbec
rock <shirock.tw@gmail.com>
2021-08-05 06:52:23 +0800

    分離 string polyfill.

--- ext/string-ext.js
+++ ext/string-ext.js
@@ -67,25 +67,3 @@ String.prototype.interpolate2 = function()
         return (args[k] ? args[k] : matched);
     });
 }
-
-// Polyfill for IE8.
-if (!String.prototype.trim) {
-String.prototype.trim = function(){
-    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
-}
-}
-
-if (!String.prototype.startsWith) {
-String.prototype.startsWith = function(search, pos) {
-    return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-};
-}
-
-if (!String.prototype.endsWith) {
-String.prototype.endsWith = function(search, this_len) {
-    if (this_len === undefined || this_len > this.length) {
-        this_len = this.length;
-    }
-    return this.substring(this_len - search.length, this_len) === search;
-};
-}
